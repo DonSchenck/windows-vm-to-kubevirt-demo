45f35e0613eb64873f50febaf37ba55defc1129e
Don Schenck <don.schenck@gmail.com>
2018-03-07 12:04:00 -0500

    Updated install_wingtiptoys

--- install_wingtiptoys.ps1
+++ install_wingtiptoys.ps1
@@ -1,2 +1,2 @@
 # Download and unpack WingtipToys
-(New-Object System.Net.WebClient).DownloadFile("https://code.msdn.microsoft.com/Getting-Started-with-221c01f5","C:\Users\Administrator\wingtiptoys.zip")
+(New-Object System.Net.WebClient).DownloadFile("https://code.msdn.microsoft.com/Getting-Started-with-221c01f5/file/107941/11/Getting Started with ASP.NET 4.5 Web Forms and Visual Studio 2013 - Wingtip Toys.zip","C:\Users\Administrator\wingtiptoys.zip")
