6ba52cd5347c2041119b04f3dfd2baa9bcb8164a
Don Schenck <don.schenck@gmail.com>
2018-03-07 12:00:13 -0500

    Added WingTipToys installation script.

---
+++ install_wingtiptoys.ps1
@@ -0,0 +1,2 @@
+# Download and unpack WingtipToys
+(New-Object System.Net.WebClient).DownloadFile("https://code.msdn.microsoft.com/Getting-Started-with-221c01f5","C:\Users\Administrator\wingtiptoys.zip")
